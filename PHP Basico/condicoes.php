7af702f7c5e2f1c59d7771391248dd4aebc5708a
LUCIO SANTOS <Luciosantospro@gmail.com>
2023-07-03 06:46:26 -0300

    Atualização aula condições

--- PHP Basico/condicoes.php
+++ PHP Basico/condicoes.php
@@ -7,4 +7,17 @@
 		echo "Hello World";
 	}
 
+	echo "<br>";
+
+	$var1 = 10;
+	$var2 = 20;
+
+	if ($var1 == $var2) {
+		echo "As variaveis são iguais";
+	}else if ($var1 > $var2){
+		echo "A variavel 1 é maior que a 2";
+	}else{
+		echo "As variaveis são diferentes";
+	}
+
 ?>
