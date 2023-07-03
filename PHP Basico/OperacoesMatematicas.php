35c42852740ec75ff22d48f10047bd11bb93cf15
LUCIO SANTOS <Luciosantospro@gmail.com>
2023-07-03 06:42:51 -0300

    Novas aulas

--- PHP Basico/OperacoesMatematicas.php
+++ PHP Basico/OperacoesMatematicas.php
@@ -1,4 +1,20 @@
 <?php 
+	$numero1 = 5+5;
+	$numero2 = 10-4;
+
+	echo "Resultado da primeira operação: $numero1 e o resultado da segunda operação $numero2";
+
+	$numero3 = $numero2 + $numero1;
+
+	echo "<br> Resultado da suma do numero 1 e numero 2 $numero3";
+
+	$expressao = 10 + 10 + 50 + 27;
+
+	echo "<br> $expressao";
+
+	$expressao1 = 40 +2 * 21;
+
+	echo "<br> $expressao1";
 
 	
 
---
+++ PHP Basico/condicoes.php
@@ -0,0 +1,10 @@
+<?php 
+
+	$nome1 = "Lucio";
+	$nome2 = "Lucio";
+
+	if ($nome1 == $nome2) {
+		echo "Hello World";
+	}
+
+?>
